e3472b4ef9de8b87baac861ddd0aa82cd2a55254
JoyfulMagician <joyfulmagician@gmail.com>
2023-11-28 19:44:57 +0100

    create auth

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,6 +1,9 @@
 import { ConnectOptions } from "mongodb";
 import mongoose from "mongoose";
+
 export const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI!);
   console.log("MongoDb Connected");
 };
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use routes
+app.use(routes);
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
