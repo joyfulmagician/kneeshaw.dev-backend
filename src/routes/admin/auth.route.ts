283b3e1e0f780488066a06f0083388ae161f7a11
JoyfulMagician <joyfulmagician@gmail.com>
2023-12-01 10:42:44 +0100

    create login api

--- src/routes/admin/auth.route.ts
+++ src/routes/admin/auth.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
+
 import authController from "../../controllers/admin/auth.controller";
 
 const router = express.Router();
 
-router.get("/", authController.getHello);
+router.route("/login").post(authController.login);
 
 export default router;
