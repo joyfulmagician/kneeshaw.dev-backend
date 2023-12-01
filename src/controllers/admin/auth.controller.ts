f62fd3ea0ded77683b6a093b260dd9572ac1c89e
truthfuldev <truthfuldev@gmail.com>
2023-12-01 10:42:44 +0100

    create login api

--- src/controllers/admin/auth.controller.ts
+++ src/controllers/admin/auth.controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 
-function getHello(_req: Request, res: Response, _next: NextFunction) {
-  res.send("Hello World!");
+import { User } from "../../models/user.model";
+
+async function login(req: Request, _res: Response, _next: NextFunction) {
+  const { email, password } = req.body;
+
+  console.log("email: ", email);
+  console.log("password: ", password);
+
+  try {
+    const user = await User.findOne({
+      email,
+      password
+    });
+    console.log("user: ", user);
+  } catch (error) {
+    console.error("admin auth.controller login error: ", error);
+  }
 }
 
-export default { getHello };
+export default { login };
