f4a2d18733c334f707ee66b9a43422f54115651b
truthfuldev <truthfuldev@gmail.com>
2023-12-01 09:41:01 +0100

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 import authRoutes from "./auth.route";
 
+import adminRoutes from "./admin";
+
 const router = express.Router();
 
 router.use("/api/auth", authRoutes);
+
+router.use("/api/admin", adminRoutes);
 
 export default router;
