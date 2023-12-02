8b547e6eac0a6d0f0fea3b43695619c58bb38dcf
JoyfulMagician <joyfulmagician@gmail.com>
2023-12-02 11:03:59 +0100

    add job routes

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 import authRoutes from "./auth.route";
 import jobSkillRoutes from "./job.skill.route";
+import jobServiceRoutes from "./job.service.route";
+import jobDatabaseRoutes from "./job.database.route";
 
 import adminMiddleware from "../../middleware/admin.middleware";
 
@@ -10,5 +12,7 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 
 router.use("/job/skill", adminMiddleware, jobSkillRoutes);
+router.use("/job/service", adminMiddleware, jobServiceRoutes);
+router.use("/job/database", adminMiddleware, jobDatabaseRoutes);
 
 export default router;
