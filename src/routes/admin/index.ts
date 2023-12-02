2005a1148e36eae8f879fe84eaa4d9ac6aba78e1
truthfuldev <truthfuldev@gmail.com>
2023-12-02 11:11:56 +0100

    complete period

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -5,6 +5,7 @@ import jobSkillRoutes from "./job.skill.route";
 import jobServiceRoutes from "./job.service.route";
 import jobDatabaseRoutes from "./job.database.route";
 import jobScopeRoutes from "./job.scope.route";
+import jobPeriodRoutes from "./job.period.route";
 
 import adminMiddleware from "../../middleware/admin.middleware";
 
@@ -16,5 +17,6 @@ router.use("/job/skill", adminMiddleware, jobSkillRoutes);
 router.use("/job/service", adminMiddleware, jobServiceRoutes);
 router.use("/job/database", adminMiddleware, jobDatabaseRoutes);
 router.use("/job/scope", adminMiddleware, jobScopeRoutes);
+router.use("/job/period", adminMiddleware, jobPeriodRoutes);
 
 export default router;
