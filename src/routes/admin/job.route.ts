585e7c30122fec9b3390fe03d13598df653d79f8
JoyfulMagician <joyfulmagician@gmail.com>
2023-12-02 10:31:01 +0100

    create job skill

--- src/routes/admin/job.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import jobController from "../../controllers/admin/job.controller";
-
-const router = express.Router();
-
-router.route("/hello").post(jobController.hello);
-
-export default router;
