086a4ba6f49cd794ee1ff22bdb12f9354c7c2873
truthfuldev <truthfuldev@gmail.com>
2023-12-03 15:34:14 +0100

    refactor job budget

--- src/routes/admin/job.budget.route.ts
+++ src/routes/admin/job.budget.route.ts
@@ -8,6 +8,5 @@ router.post("/", jobBudgetController.createJobBudget);
 router.get("/", jobBudgetController.getAllJobBudgets);
 router.get("/:id", jobBudgetController.getJobBudget);
 router.put("/:id", jobBudgetController.updateJobBudget);
-// router.delete("/:id", jobBudgetController.deleteJobBudget);
 
 export default router;
